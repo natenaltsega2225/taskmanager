64ad4151b33da5ee6055db96d789e23819203fbf
Natenal Tsega <nati2gashaw@gmail.com>
2022-04-15 00:38:15 -0500

    inital commit

---
+++ src/components/Todo.js
@@ -0,0 +1,45 @@
+import React, {useState} from 'react';
+import TodoForm from './TodoForm';
+import {RiCloseCircleLine} from 'react-icons/ri';
+import {TiEdit} from 'react-icons/ti';
+
+
+// variable declared with const and can not be changed 
+const Todo = ({todos, completeTodo, removeTodo, upadateTodo}) => {
+     // useState tracks a state the current fuction component
+    const [edit, setEdit] = useState({
+        id: null,
+        value: ''     
+         });
+
+    const submitUpdate = value => {
+        upadateTodo(edit.id, value);
+        setEdit ({
+            id:null,
+            value:''
+       });
+    };
+
+    if (edit.id){
+        return <TodoForm edit={edit} onSubmit={submitUpdate}/>;
+    }
+    return todos.map((todo, index) => (
+        <div
+            className={todo.isComplete ? 'todo-row complete' : 'todo-row'}
+            key={index}>
+        <div key={todo.id} onClick={() =>completeTodo(todo.id)}>
+            {todo.text}
+        </div>
+        <div className='icons'>
+            <RiCloseCircleLine onClick={() => removeTodo(todo.id)}
+            className='delete-icon' />
+   <TiEdit onClick={() => setEdit({id:todo.id, value:todo.text})}
+            className='edit-icon' />      
+        </div>
+
+        </div>
+    ));
+    };
+
+    export default Todo;
+    
